3c55dbb328298697b736be15b0e2b173d50abb46
Damian <dmedina12@me.com>
2018-09-19 14:26:42 -0500

    Added HTML/CSS/ JS
    Not seeing the author/quote display when button is clicked...here we go..

---
+++ script.js
@@ -0,0 +1,62 @@
+const simpleQuotes = [
+
+
+{
+
+name: 'Gucci Mane',
+quote: 'If a man does not have sauce, then he is lost. But the same man can get lost in the sauce.'
+
+
+}
+
+{
+
+name: 'Elon Musk',
+quote: 'When something is important enough, you do it even if the odds are not in your favor.'
+
+
+}
+{
+
+name: 'Kanye West',
+quote: 'People always tell you Be humble. Be humble. When was the last time someone told you to be amazing? Be great! Be great! Be awesome! Be awesome!'
+
+
+}
+{
+
+name: 'Frank Ocean',
+quote: 'The Internet made fame wack and anonymity cool.'
+
+
+}
+{
+
+name: 'Sten Nadolny, Die Entdeckung der Langsamkeit',
+quote: 'Learning and seeing are more important than education. '
+
+
+}
+
+
+
+
+
+]
+
+const quoteBtn = document.querySelector('#quoteBtn');
+const quoteAuthor = document.querySelector ('#quoteAuthor');
+const quote = document.querySelector('#quote');
+
+quoteBtn.addEventListener('click',displayQuote);
+
+function displayQuote (){
+
+
+let number = Math.floor(Math.random()*quotes.length);
+
+//shows the author and quote
+quoteAuthor.innerHTML = quotes[number].name;
+quote.innerHTML = quotes[number].quote;
+
+}
